dc337c1782e48aa46e2fed89953a5c5a3d0efc62
Aleksey <134486666+Aleky63@users.noreply.github.com>
2025-04-11 12:54:47 +0300

    01_11.04

--- cabsda/src/App.tsx
+++ cabsda/src/App.tsx
@@ -5,8 +5,8 @@ import Rating from "./components/Rating/Rating";
 function App() {
   return (
     <div>
-      <PageTitle title={"This is 555555"} />
-      <PageTitle title={"This is 030303"} />
+      <PageTitle title={"This is 55555"} />
+      <PageTitle title={"This is 0303"} />
       <input />
       <input type="password" />
       <input type="date" />
